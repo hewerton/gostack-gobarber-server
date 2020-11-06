68f45225a22e17a6b56e292798fae32c6cf6156e
Hewerton Oliveira <hewerton.comp@gmail.com>
2020-11-06 16:34:03 -0300

    Appointment model and repository created.

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+interface AppointmentCreateDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public create({ provider, date }: AppointmentCreateDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const foundAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return foundAppointment || null;
+  }
+}
+
+export default AppointmentsRepository;
