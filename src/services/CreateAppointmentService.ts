8dd23527ef1c0403f3a1087b33d3b346d33f5fa5
Hewerton Oliveira <hewerton.comp@gmail.com>
2020-11-06 16:35:01 -0300

    Service to create a new Appointment.

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentRepository: AppointmentRepository;
+
+  constructor(appointmentRepository: AppointmentRepository) {
+    this.appointmentRepository = appointmentRepository;
+  }
+
+  public execute({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const foundAppointmentInSameDate = this.appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (foundAppointmentInSameDate) {
+      throw Error('This appointment is already booked.');
+    }
+
+    const appointment = this.appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
