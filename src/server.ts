1bf31b758e6d0943f6b697211f4883aaacad56a0
Hewerton Oliveira <hewerton.comp@gmail.com>
2020-11-11 18:12:12 -0300

    Serving static files.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/uploadConfig';
 
 import './database';
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use('/files', express.static(uploadConfig.destination));
 
 app.listen(3333, () => {
   console.log('Server started!');
